484ab1e2cbbe84b14379a8d798488529120ea230
peanutbutterclassic <rano.r.s.fisher@gmail.com>
2022-01-15 20:22:57 +0000

    Create battle grids for game area

--- run.py
+++ run.py
@@ -10,9 +10,45 @@ def battle_intro():
 def enter_battle_perimeter():
     print("YOU ARE ENTERING THE BATTLE PERIMETER!\n")
 
+def create_grid():
+    """Will create a 10x10 grid and randomly place down ships
+       of different sizes in different directions"""
+    global grid
+    global grid_size
+    global num_of_ships
+    global ship_positions
+    global linked_words
+
+    random.seed(time.time())
+
+    rows, cols = (grid_size, grid_size)
+
+    grid = []
+    for r in range(rows):
+        row = []
+        for c in range(cols):
+            row.append(".")
+        grid.append(row)
+
+    num_of_ships_placed = 0
+
+    ship_positions = []
+
+    while num_of_ships_placed != num_of_ships:
+        random_row = random.randint(0, rows - 1)
+        random_col = random.randint(0, cols - 1)
+        direction = random.choice(["left", "right", "up", "down"])
+        ship_size = random.randint(3, 5)
+        if try_to_place_ship_on_grid(random_row, random_col, direction, ship_size):
+            num_of_ships_placed += 1
+            linked_word = get_random_word(ship_size)
+            linked_words.append({'row': random_row, 'col': random_col, 'word': linked_word})
+
 def main():
     battle_intro()
     enter_battle_perimeter()
+    create_grid()
+    
 
 
 if __name__ == '__main__':
