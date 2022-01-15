cb779c8ef45360efb6a20c60782cc2914eb90c2d
peanutbutterclassic <rano.r.s.fisher@gmail.com>
2022-01-15 19:21:48 +0000

    Add preliminary questions at game start

---
+++ run.py
@@ -0,0 +1,15 @@
+def battle_intro():
+    print("----------Welcome to BattleScrabble!----------")
+    name = input("What's your name? \n")
+    age = int(input("What's your age: \n"))
+    if age >= 18:
+        print("You are eligible to enter the battlefield.")
+    else:
+        print(f"Hello {name.capitalize()}, aren't you too young.")
+
+def main():
+    battle_intro()
+
+
+if __name__ == '__main__':
+    main()
