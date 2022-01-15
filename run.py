dea545cde3f6ce7c7369953d16bf73bf3f208fd8
peanutbutterclassic <rano.r.s.fisher@gmail.com>
2022-01-15 21:30:33 +0000

    Add function for selecting grids and placing ships

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+import random
+import time
+
 def battle_intro():
     print("----------Welcome to BattleScrabble!----------")
     name = input("What's your name? \n")
@@ -10,6 +13,31 @@ def battle_intro():
 def enter_battle_perimeter():
     print("YOU ARE ENTERING THE BATTLE PERIMETER!\n")
 
+def validate_grid_and_place_ship(start_row, end_row, start_col, end_col):
+    """
+    Will check the row or column to see if it is safe to place a ship there
+    """
+    global grid
+    global ship_positions
+
+    all_valid = True
+    for r in range(start_row, end_row):
+        for c in range(start_col, end_col):
+            if grid[r][c] != ".":
+                all_valid = False
+                break
+    if all_valid:
+        ship_positions.append([start_row, end_row, start_col, end_col])
+        for r in range(start_row, end_row):
+            for c in range(start_col, end_col):
+                grid[r][c] = "O"
+    return all_valid
+
+def try_to_place_ship_on_grid(row, col, direction, length):
+    global grid_size
+    pass
+    return validate_grid_and_place_ship(0, 0, 0, 0)
+
 def create_grid():
     """Will create a 10x10 grid and randomly place down ships
        of different sizes in different directions"""
@@ -17,7 +45,6 @@ def create_grid():
     global grid_size
     global num_of_ships
     global ship_positions
-    global linked_words
 
     random.seed(time.time())
 
@@ -41,8 +68,6 @@ def create_grid():
         ship_size = random.randint(3, 5)
         if try_to_place_ship_on_grid(random_row, random_col, direction, ship_size):
             num_of_ships_placed += 1
-            linked_word = get_random_word(ship_size)
-            linked_words.append({'row': random_row, 'col': random_col, 'word': linked_word})
 
 def print_grid():
     """Will print the grid with rows A-J and columns 0-9"""
@@ -70,12 +95,30 @@ def print_grid():
         print(str(i), end=" ")
     print("")
 
+# Global variable for grid
+grid = [[]]
+# Global variable for grid size
+grid_size = 10
+# Global variable for number of ships to place
+num_of_ships = 3
+# Global variable for bullets left
+bullets_left = 50
+# Global variable for game over
+game_over = False
+# Global variable for number of ships sunk
+num_of_ships_sunk = 0
+# Global variable for ship positions
+ship_positions = [[]]
+# Global variable for alphabet
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 def main():
     battle_intro()
     enter_battle_perimeter()
     create_grid()
+    print_grid()
     
-
+    
 
 if __name__ == '__main__':
     main()
